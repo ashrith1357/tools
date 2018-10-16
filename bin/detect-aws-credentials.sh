a7755e64c77052244c5ebe006e6106b3904c3004
ashrith1357 <ramu.siemens@gmail.com>
2018-10-16 11:52:20 -0700

    Update detect-aws-credentials.sh

--- bin/detect-aws-credentials.sh
+++ bin/detect-aws-credentials.sh
@@ -12,59 +12,54 @@ fi
 # Redirect output to stderr.
 exec 1>&2
 
-# Check changed files for an AWS keys
-FILES=$(git diff --cached --name-only $against)
-
 if [ -n "$FILES" ]; then
    echo "Verifying the terraform format..please wait"
-   terraform fmt
+    terraform fmt
 fi
 echo
-echo "*********************************************************"
+echo "*****************************"
 echo
-
 if [ -n "$FILES" ]; then
    echo "Validating the terraform code..please wait"
     terraform validate -check-variables=false
-
     if [ $? != 0 ]
     then
       echo "Code failed the teraform validation" >&2
       exit 1
     fi
 fi
-
 echo
-echo "*********************************************************"
+echo "*****************************"
 echo
-
 if [ -n "$FILES" ]; then
     echo "Validating the terraform code for AWS Secrets..please wait"
-    
     ACCESS_KEY=$(grep -rE --line-number 'access_key' $FILES)
     SECRET_KEY=$(grep -rE --line-number 'secret_key' $FILES)
-
+    if [ -n "$ACCESS_KEY" ] || [ -n "$SECRET_KEY" ]; then
+        exec < /dev/tty # Capture input
+        echo "=========== Possible AWS Access Key IDs ==========="
+        echo "${ACCESS_KEY}"
+        echo ""
         if [ -n "$ACCESS_KEY" ] || [ -n "$SECRET_KEY" ]; then
-            
+            exec < /dev/tty # Capture input
             echo "=========== Possible AWS Access Key IDs ==========="
             echo "${ACCESS_KEY}"
             echo ""
-
+        echo "=========== Possible AWS Secret Access Keys ==========="
+        echo "${SECRET_KEY}"
+        echo ""
             echo "=========== Possible AWS Secret Access Keys ==========="
             echo "${SECRET_KEY}"
             echo ""
-
+        while true; do
             while true; do
-                    echo "One of secret credentials found so exiting..please check"
-                    echo
-                    echo "*********************************************************"
-                    echo
+                exit 1;
                     exit 1;
-
+        done
             done
-
         fi
     fi
+fi
 
 
 exit 0
